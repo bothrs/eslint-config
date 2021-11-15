da31c377b915b6ac8b8ce7a5645d6b4fcbc9d937
Fabian Meul <fabian.meul@gmail.com>
2021-11-15 11:51:03 +0100

    feat(a11y): Add recommended for react and react-native

--- src/react.js
+++ src/react.js
@@ -1,9 +1,10 @@
 module.exports = {
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
   extends: [
     'plugin:react/recommended',
     'plugin:react/jsx-runtime', // Assume React 17
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
