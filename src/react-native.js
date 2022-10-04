7c0d0ce585d39182a54b6a108dc3931eedf0366c
TijsM <tijs.martens.tijs@gmail.com>
2022-10-04 16:37:24 +0200

    feat: add fontawesome import rules

--- src/react-native.js
+++ src/react-native.js
@@ -4,4 +4,21 @@ module.exports = {
   env: {
     'react-native/react-native': true,
   },
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          '@fortawesome/free-solid-svg-icons',
+          '@fortawesome/free-brands-svg-icons',
+          '@fortawesome/free-regular-svg-icons',
+          '@fortawesome/pro-solid-svg-icons',
+          '@fortawesome/pro-regular-svg-icons',
+          '@fortawesome/pro-light-svg-icons',
+          '@fortawesome/pro-duotone-svg-icons',
+          '@fortawesome/sharp-solid-svg-icons',
+        ],
+      },
+    ],
+  },
 }
