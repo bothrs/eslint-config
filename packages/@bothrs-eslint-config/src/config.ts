7e093243a8d8ce2126382f2efa87c7dcf9141efe
Fabian Meul <fabian.meul@gmail.com>
2022-11-08 15:03:54 +0100

    feat: Allow hooks with .tsx extension

--- packages/@bothrs-eslint-config/src/config.ts
+++ packages/@bothrs-eslint-config/src/config.ts
@@ -67,7 +67,7 @@ module.exports = {
           /\w+.(spec|test).([jt])sx?$/, // .spec.tsx files
           /\w+.styled.([jt])sx?$/, // .styled.tsx files
           /\w+.([jt])s$/, // regular .ts files
-          /^use([A-Z])\w+.(j|t)s$/, // useHook.ts
+          /^use([A-Z])\w+.(j|t)sx?$/, // useHook.ts
         ],
       },
     ],
