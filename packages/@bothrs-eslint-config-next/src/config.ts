a385399828b026f7541d91356a0abd101450227a
Jacco Goris <contact@jaccogoris.com>
2023-04-20 11:33:53 +0200

    fix: improved next preset and fixed readme links

--- packages/@bothrs-eslint-config-next/src/config.ts
+++ packages/@bothrs-eslint-config-next/src/config.ts
@@ -13,5 +13,23 @@ module.exports = {
     'prettier', // Make sure to put it last, so it gets the chance to override other configs.
   ],
   rules: {
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          res: false,
+          req: false,
+        },
+      },
+    ],
   },
+  overrides: [
+    // Next.js needs default exports for pages and API points
+    {
+      files: ['pages/*', 'pages/api/*'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
 } as Linter.BaseConfig
